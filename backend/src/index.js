6c6341fa7185b71d4e80808a4fddaa7e5fc5a3d0
soorajas1210 <soorajasofficial@gmail.com>
2025-01-15 12:09:45 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: " http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: " + PORT);
+  connectDB();
+});
