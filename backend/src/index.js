da55fda0320f9a6077dfb0859dc8c0f978053890
soorajas1210 <soorajasofficial@gmail.com>
2025-01-17 22:50:01 +0530

    minor update

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,11 +28,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../../frontend", "dist", "indexedDB.html")
-    );
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "indexedDB.html"));
   });
 }
 server.listen(PORT, () => {
